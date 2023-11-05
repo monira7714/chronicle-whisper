63e9318dae6038d4581d048a0b684b85734d71fb
monira7714 <monirabegum7714@gmail.com>
2023-11-05 16:24:53 +0600

    The favicon and website name is added

--- src/shared/NavBar/NavBar.jsx
+++ src/shared/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 // import { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import { AuthContext } from "../../provider/AuthProvider";
 // import { getAuth } from "firebase/auth";
 import './NavBar.css'
